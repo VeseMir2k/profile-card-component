dd7c65480e0e6660c618fb6742c5a4f3463651c8
Adam Stępień <vesemir2k@gmail.com>
2024-06-02 10:43:35 +0200

    🔧 (.prettierrc): update prettier configuration to include new formatting options 🗑️ (favicon-32x32.png): delete unused favicon image 📝 (index.html): update body class and add a new background pattern div 🔧 (tailwind.config.js): extend theme with new background image options for patterns

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        patternTop: 'url("./images/bg-pattern-top.svg")',
+        patternBottom: 'url("./images/bg-pattern-bottom.svg")',
+        patternCard: 'url("./images/bg-pattern-card.svg")',
+      },
+    },
   },
   plugins: [],
 }
